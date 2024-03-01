9d1aa8665b76459997f2d85b8ec4269b05e641b6
puttipong95 <pgmoney1955@gmail.com>
2024-03-01 19:31:00 +0700

    Update Edit

--- src/components/FilterTodo/FilterTodo.tsx
+++ src/components/FilterTodo/FilterTodo.tsx
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 type FilterTodoProps = {
   getTodoFilterValue: (filterValue: string) => void;
 };
 
 const FilterTodo = ({ getTodoFilterValue }: FilterTodoProps) => {
-  return <div>FilterTodo</div>;
+  const [filterTodoVal, setFilterTodoVal] = useState("all");
+
+// This event handler updates current select option and passes currently selected option value to App component  
+const handleFilterTodoChanges = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setFilterTodoVal(e.target.value);
+    getTodoFilterValue(e.target.value);
+  };
+  return (
+    <select
+      onChange={handleFilterTodoChanges}
+      value={filterTodoVal}
+      className="filter-todo"
+    >
+      <option value="all">All</option>
+      <option value="completed">Completed</option>
+    </select>
+  );
 };
 
 export default FilterTodo;
